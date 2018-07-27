f751f26273982edd82736326f4914c767ce01ca9
wangfengyuan <wangfengyuan@cn.net.ntes>
2018-07-27 19:41:54 +0800

    modify README.md

--- indexOrigin.js
+++ indexOrigin.js
@@ -8,21 +8,24 @@ let appID = `wxec6fa9e9bc03d885`;
 //appsecret
 let appSerect = `4c8a0d14cff08959b4e17334cabf9cf0`;
 //点击授权后重定向url地址
-let redirectUrl = `/auth`;
+let redirectUrl = `/getUserInfo`;
 let host = `http://127.0.0.1:3000`;
 //微信授权api,接口返回code,点击授权后跳转到重定向地址并带上code参数
 let authorizeUrl = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appID}&redirect_uri=` +
     `${host}${redirectUrl}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
 
-app.get("/index", function(req, res) {
+app.get("/login", function(req, res) {
+    res.sendFile(path.resolve(__dirname,'login.html'));
+});
+
+app.get("/auth", function(req, res) {
     res.writeHead(302, {
         'Location': authorizeUrl
     });
     res.end();
 });
 
-var user = '';
-app.get("/auth", function(req, res) {
+app.get("/getUserInfo", function(req, res) {
     let code = req.query.code;
     let getaccess = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=` +
         `${appID}&secret=${appSerect}&code=${code}&grant_type=authorization_code`;
